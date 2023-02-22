9dfc0f549e398cd1a2ad3091725a01f2fa39e669
Sergey Klyusov <donot@email.me>
2023-02-22 23:36:41 +0500

    backing up

---
+++ file2red/file2red.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "encoding/gob"
+
+  "github.com/davecgh/go-spew/spew"
+  "github.com/gomodule/redigo/redis"
+  . "github.com/ShyLionTjmn/mapper/mapaux"
+)
+
+const REDIS_SOCKET="/tmp/redis.sock"
+const REDIS_DB="0"
+
+func init() {
+  gob.Register(M{})
+  gob.Register([][]uint8{})
+}
+
+func main() {
+  var red redis.Conn
+  fmt.Print()
+  _ = spew.Sdump(nil)
+
+  defer func() { if red != nil { red.Close() } } ()
+
+  var err error
+
+  if len(os.Args)  < 2 || (os.Args[1] == "-y" && len(os.Args)  < 3) {
+    fmt.Println("Usage: ", os.Args[0], " [-y] in_file [key1 ...]\n"+
+                "\t-y\t - Really set values\n\tkey\t - keys list to set\n",
+    )
+  }
+
+  opt_y := false
+  var opt_f string
+
+  var keys []string
+
+  if os.Args[1] == "-y" {
+    opt_y = true
+    opt_f = os.Args[2]
+    keys = os.Args[3:]
+  } else {
+    opt_f = os.Args[1]
+    keys = os.Args[2:]
+  }
+
+  red, err = RedisCheck(red, "unix", REDIS_SOCKET, REDIS_DB)
+
+  if err != nil {
+    panic(err)
+  }
+
+  var in M
+
+  var fh *os.File
+  if fh, err = os.Open(opt_f); err != nil { panic(err) }
+  defer fh.Close()
+
+  dec := gob.NewDecoder(fh)
+  if err = dec.Decode(&in); err != nil { panic(err) }
+
+  if in == nil { return }
+
+  for key, data := range in {
+    fmt.Print(key, " ")
+    if len(keys) == 0 || IndexOf(keys, key) >= 0 {
+
+      rargs := redis.Args{}.Add(key)
+      rcmd := ""
+
+      switch(data.(M)["type"].(string)) {
+      case "string":
+        rcmd = "SET"
+        rargs.Add(data.(M)["data"])
+      case "hash":
+        rcmd = "HSET"
+        for _, arg := range data.(M)["data"].([][]uint8) {
+          rargs.Add(arg)
+        }
+      case "list":
+        rcmd = "RPUSH"
+        for _, arg := range data.(M)["data"].([][]uint8) {
+          rargs.Add(arg)
+        }
+      default:
+        fmt.Println("Unsupported type")
+        continue
+      }
+      fmt.Println(data.(M)["type"])
+      if opt_y {
+        if _, err = red.Do("DEL", key); err != nil { panic(err) }
+        if _, err = red.Do(rcmd, rargs); err != nil { panic(err) }
+      }
+    }
+  }
+}
---
+++ red2file/red2file.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "encoding/gob"
+
+  "github.com/davecgh/go-spew/spew"
+  "github.com/gomodule/redigo/redis"
+  . "github.com/ShyLionTjmn/mapper/mapaux"
+)
+
+const REDIS_SOCKET="/tmp/redis.sock"
+const REDIS_DB="0"
+
+func init() {
+  gob.Register(M{})
+  gob.Register([][]uint8{})
+}
+
+func main() {
+  var red redis.Conn
+  var result interface{}
+
+  fmt.Print()
+  _ = spew.Sdump(nil)
+
+  defer func() { if red != nil { red.Close() } } ()
+
+  var err error
+
+  if len(os.Args)  < 3 {
+    fmt.Println("Usage: ", os.Args[0], " out_file key1 [key2 ...]")
+  }
+
+  red, err = RedisCheck(red, "unix", REDIS_SOCKET, REDIS_DB)
+
+  if err != nil {
+    panic(err)
+  }
+
+  out := M{}
+
+  for i, arg := range os.Args {
+    if i < 2 { continue }
+    cursor := "0"
+    for {
+      result, err = red.Do("SCAN", cursor, "MATCH", arg, "COUNT", 1000)
+      if err != nil { panic(err) }
+      cursor = string(result.([]interface{})[0].([]uint8))
+      values := result.([]interface{})[1].([]interface{})
+      for _, vali := range values {
+        key := string(vali.([]uint8))
+        var key_type string
+        if key_type, err = redis.String(red.Do("TYPE", key)); err != nil { panic(err) }
+        fmt.Println(key, key_type)
+
+        var save_data interface{}
+
+        switch key_type {
+        case "string":
+          save_data, err = redis.Bytes(red.Do("GET", key))
+        case "hash":
+          save_data, err = redis.ByteSlices(red.Do("HGETALL", key))
+        case "list":
+          save_data, err = redis.ByteSlices(red.Do("LRANGE", key, 0, -1))
+        default:
+          continue
+        }
+
+        if err != nil { panic(err) }
+
+        out[key] = M{"type": key_type, "data": save_data}
+
+      }
+      if cursor == "0" { break }
+    }
+  }
+
+  if len(out) != 0 {
+    var fh *os.File
+    if fh, err = os.OpenFile(os.Args[1], os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0o660); err != nil { panic(err) }
+    defer fh.Close()
+
+    enc := gob.NewEncoder(fh)
+    if err = enc.Encode(out); err != nil { panic(err) }
+  }
+}
